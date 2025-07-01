ae8249197ad416b78cb0d4e508e4962854ad6127
rinceri <rinceri@protonmail.com>
2025-07-01 19:22:48 +0100

    fix module imports

--- src/register.py
+++ src/register.py
@@ -54,7 +54,7 @@ class VerticalButtons(Vertical):
     @on(Button.Pressed, "#back_button")
     def on_back(self):
         # to prevent cycling import
-        from src.main import LoginScreen
+        from main import LoginScreen
 
         self.app.switch_screen(LoginScreen())
 
