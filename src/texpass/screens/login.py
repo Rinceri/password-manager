c99f3bf9120def14d75cade6f937cc42fecb0d35
rinceri <rinceri@protonmail.com>
2025-09-07 15:49:07 +0100

    quit app with escape

--- src/texpass/screens/login.py
+++ src/texpass/screens/login.py
@@ -1,5 +1,7 @@
 from textual.app import ComposeResult
 from textual.screen import Screen
+from textual.binding import Binding
+from textual.widgets import Footer
 
 from texpass.widgets.login import LoginWidget
 from texpass.helper.switch_message import Switch
@@ -12,6 +14,10 @@ class LoginScreen(Screen):
     The login screen. First screen when opening the app
     """
     CSS_PATH = "../styles/login.tcss"
+    BINDINGS = [
+        Binding("escape", "exit_app", "Exit app", priority=True),
+    ]
+
 
     def __init__(self, controller: LoginController, switcher: ScreenController):
         self.controller = controller
@@ -22,9 +28,12 @@ class LoginScreen(Screen):
         login_widget = LoginWidget(self.controller)
         
         yield login_widget
-
+        
     def on_switch(self, message: Switch):
         if message.switch_else:
             self.switcher.to_register()
         else:
             self.switcher.to_table(message.account)
+
+    def action_exit_app(self):
+        self.app.exit()
